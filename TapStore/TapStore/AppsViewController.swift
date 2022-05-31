e895449c22a815d61b1fb00d1708c949bbd00ea2
ucwealth <ucheokeke16@gmail.com>
2022-05-31 17:24:51 +0100

    configure snapshot for diffable data source

--- TapStore/TapStore/AppsViewController.swift
+++ TapStore/TapStore/AppsViewController.swift
@@ -11,6 +11,7 @@ class AppsViewController: UIViewController {
     let sections = Bundle.main.decode([Section].self, from: "appstore.json")
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, App>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,8 +19,36 @@ class AppsViewController: UIViewController {
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         collectionView.backgroundColor = .systemBackground
         view.addSubview(collectionView)
+        
+        collectionView.register(FeaturedCell.self, forCellWithReuseIdentifier: FeaturedCell.reuseIdentifier)
+        
+        createDataSource()
+        reloadData()
     }
 
+    func configure<T: SelfConfiguringCell>(_ cellType: T.Type, with app: App, for indexPath: IndexPath) -> T {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellType.reuseIdentifier, for: indexPath) as? T else { fatalError("Unable to dequeue \(cellType)") }
+        cell.configure(with: app)
+        return cell
+    }
 
+    func createDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, App>(collectionView: collectionView) { collectionView, indexPath, app in
+            switch self.sections[indexPath.section].type {
+            default:
+                return self.configure(FeaturedCell.self, with: app, for: indexPath)
+            }
+        }
+    }
+    
+    func reloadData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, App>()
+        snapshot.appendSections(sections)
+        
+        for section in sections {
+            snapshot.appendItems(section.items, toSection: section)
+        }
+        dataSource?.apply(snapshot)
+    }
 }
 
